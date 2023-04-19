2f5bcd1d0ca1fb9c56df86cd7d67acf049683453
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-04-19 16:34:41 +0300

    input field added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from "react";
+import './styles/App.css';
+import Search from './icons/search.png';
+import Close from './icons/close.png';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    const [value, setValue] = useState('');
+
+    const handleKeyPress = (event) => {
+        if (event.key === 'Enter'){
+            console.log(value);
+            setValue('');
+        }
+        if (event.key === 'Escape'){
+            setValue('');
+        }
+    }
+
+    const submitSearch = () => {
+        console.log(value);
+        setValue('');
+    }
+
+    const cancelSearch = () => {
+        setValue('');
+    }
+
+    return (
+        <div className="App">
+            <input
+                type={'text'}
+                className={'input'}
+                value={value}
+                onChange={(e) => setValue(e.target.value)}
+                placeholder={'Поиск...'}
+                onKeyDown={handleKeyPress}
+            />
+            <input type={'image'} src={Search} className={'search_icon'} onClick={submitSearch}/>
+            {value !== '' &&
+                <input type={'image'} src={Close} className={'close_icon'} onClick={cancelSearch}/>
+            }
+        </div>
+    );
 }
 
 export default App;
