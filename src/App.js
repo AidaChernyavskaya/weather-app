179d7189738e25f36bf0d9282e983faf8bf0afb8
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-04-21 13:33:56 +0300

    created CardItem component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
 import React, {useState} from "react";
 import './styles/App.css';
 import SearchField from "./components/SearchField/SearchField";
+import Search from "./icons/search.png";
+import Windy from "./icons/wind.png";
+import Sunny from "./icons/sunny.png";
+import Cloudy from "./icons/clouds.png";
+import CardItem from "./components/CardItem/CardItem";
 
 function App() {
-
 
     return (
         <div className="App">
             <SearchField/>
+            <CardItem/>
         </div>
     );
 }
---
+++ src/components/CardItem/CardItem.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import Windy from "../../icons/wind.png";
+import Cloudy from "../../icons/clouds.png";
+import Sunny from "../../icons/sunny.png";
+import styles from "./CardItem.module.css";
+import cn from 'classnames';
+
+const CardItem = () => {
+
+    return (
+        <div className={cn(styles.card, styles.outline)}>
+            <h3 className={styles.card__date}>Вт, 11 апреля</h3>
+            <div className={styles.card__breakpoints}>
+                <p>Ночь</p>
+                <p>Утро</p>
+                <p>День</p>
+                <p>Вечер</p>
+            </div>
+            <div className={styles.card__icons}>
+                <input type={'image'} src={Windy}/>
+                <input type={'image'} src={Cloudy}/>
+                <input type={'image'} src={Sunny}/>
+                <input type={'image'} src={Cloudy}/>
+            </div>
+            <div className={styles.card__temperature_heading}>
+                <p>Температура воздуха, °C</p>
+            </div>
+            <div className={styles.card__temperature}>
+                <p>-1</p>
+                <p>0</p>
+                <p>+5</p>
+                <p>+25</p>
+            </div>
+        </div>
+    );
+};
+
+export default CardItem;
