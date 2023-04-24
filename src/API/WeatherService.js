b991c6dde237e17df09ee107c2ec005eafb0f2d8
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-04-24 15:16:32 +0300

    added local file for api

--- src/API/WeatherService.js
+++ src/API/WeatherService.js
@@ -3,7 +3,7 @@ import axios from "axios";
 export default class PostService {
     static async getForecast() {
         try {
-            const response = await axios.get("https://api.openweathermap.org/data/2.5/forecast?id=498817&units=metric&appid=3641ae729ccdf47aa710c24709726187&lang=ru");
+            const response = await axios.get(`https://api.openweathermap.org/data/2.5/forecast?id=498817&units=metric&appid=${process.env.REACT_APP_APPID}&lang=ru`);
             return response.data;
         } catch (e) {
             console.log(e);
