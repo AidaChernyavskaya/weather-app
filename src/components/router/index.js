b7f8033476202725e4ea103ef01e6a2e131bede8
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-05-20 18:39:05 +0300

    page About is done

--- src/components/router/index.js
+++ src/components/router/index.js
@@ -1,10 +1,11 @@
 import Navbar from "../Navbar/Navbar";
 import Forecast from "../../pages/Forecast";
 import Main from "../../pages/Main";
+import About from "../../pages/About";
 
 
 export const routes = [
-    {path: '/about', element: <h3>Hi</h3>},
+    {path: '/about', element: <About/>},
     {path: '/main', element: <Main/>},
     {path: '/forecast', element: <Forecast/>},
 ]
---
+++ src/pages/About.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <p className={'about'}>Данное приложение сделано в учебных целях...</p>
+    );
+};
+
+export default About;
