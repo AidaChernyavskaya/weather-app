485823a52ec8e51d0ab40284f1b4946307cdcbcf
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-05-25 15:26:59 +0300

    dropdown menu is added

--- src/components/SearchField/SearchField.jsx
+++ src/components/SearchField/SearchField.jsx
@@ -2,6 +2,7 @@ import React, {useState} from 'react';
 import Search from "../../icons/search.png";
 import Close from "../../icons/close.png";
 import styles from './SearchField.module.css';
+import cn from "classnames";
 
 const SearchField = ({text, setName}) => {
     const [value, setValue] = useState('');
@@ -29,15 +30,35 @@ const SearchField = ({text, setName}) => {
             <div className={styles.search_field}>
                 <input
                     type={'text'}
-                    className={styles.input}
+                    className={cn(styles.input, value.length >= 1 && styles.input__open)}
                     value={value}
                     onChange={(e) => setValue(e.target.value)}
                     placeholder={text}
                     onKeyDown={handleKeyPress}
                 />
-                <input type={'image'} src={Search} className={styles.search_icon} onClick={submitSearch}/>
+                <div className={value.length >= 1 ? styles.dropdown : styles.dropdown__close}>
+                    <hr />
+                    <ul className={styles.dropdown__menu}>
+                        <li className={styles.dropdown__item}>Example1</li>
+                        <li className={styles.dropdown__item}>Example11111111111</li>
+                    </ul>
+                </div>
+
+                <input
+                    type={'image'}
+                    src={Search}
+                    className={styles.search_icon}
+                    onClick={submitSearch}
+                    alt={'search icon'}
+                />
                 {value !== '' &&
-                    <input type={'image'} src={Close} className={styles.close_icon} onClick={cancelSearch}/>
+                    <input
+                        type={'image'}
+                        src={Close}
+                        className={styles.close_icon}
+                        onClick={cancelSearch}
+                        alt={'close icon'}
+                    />
                 }
             </div>
         </div>
