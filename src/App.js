346fce20bb7026ae29453588fff968cf5b9d2088
Aida Chernyavskaya <aidachernyavskaya@gmail.com>
2023-05-29 19:40:29 +0300

    little changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import './styles/App.css';
-import { HashRouter} from "react-router-dom";
+import {BrowserRouter} from "react-router-dom";
 import AppRouter from "./components/AppRouter";
 
 function App() {
     return (
-        <HashRouter>
+        <BrowserRouter>
             <AppRouter/>
-        </HashRouter>
+        </BrowserRouter>
     );
 }
 
